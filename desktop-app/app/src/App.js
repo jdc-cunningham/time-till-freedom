ffaa2701ee6980d50f0e5b1364f3b4abd3896568
Jacob Cunningham <jcun4128@gmail.com>
2025-11-18 19:38:28 -0600

    states

--- desktop-app/app/src/App.js
+++ desktop-app/app/src/App.js
@@ -1,29 +1,43 @@
 import React, { useState } from 'react';
 import './App.css';
 import HeaderTabs from './components/header-tabs/HeaderTabs';
+import ModalAddDebt from './components/modal-add-debt/ModalAddDebt';
 
 function App() {
   const [activeTab, setActiveTab] = useState('home');
+  const [activeModal, setActiveModal] = useState('');
 
   const renderBody = (activeTab) => {
     switch (activeTab) { // enum
-      case 'alt':
-        return <div className="Body-alt">
+      case 'home':
+        return <div className="Body-home">
 
         </div>;
       default:
-        return <div className="Body-home">
+        return <div className="Body-unknown">
+          <p>unknown body</p>
+        </div>;
+    }
+  };
 
+  const renderModal = (activeModal) => {
+    switch (activeModal) {
+      case 'add-debt':
+        return <ModalAddDebt/>;
+      default:
+        return <div className="ModalUnknown">
+          <p>unknown modal</p>
         </div>;
     }
   }
 
   return (
     <div className="App">
-      <HeaderTabs/>
-      <div className="Body">
-        { renderBody(activeTab) }
-      </div>
+      {!activeModal && <HeaderTabs/>}
+      {!activeModal && <div className="Body">
+        {renderBody(activeTab)}
+      </div>}
+      {activeModal && renderModal(activeModal)}
     </div>
   );
 }
